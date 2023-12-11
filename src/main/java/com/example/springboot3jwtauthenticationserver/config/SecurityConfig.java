1450dbe6f1174b5c07f325bce632bc81e99a7103
mamun-swe <mamun.swe.277@gmail.com>
2023-12-11 15:30:38 +0600

    Bcrypt password compare implemented.

--- src/main/java/com/example/springboot3jwtauthenticationserver/config/SecurityConfig.java
+++ src/main/java/com/example/springboot3jwtauthenticationserver/config/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.example.springboot3jwtauthenticationserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 //@EnableWebSecurity
-//@EnableMethodSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
